ba482946bdf4fd0459ea5bb161b3078bf58eec4c
odblue <odonusb@gmail.com>
2016-11-29 16:20:11 -0500

    Update smartmirror.py

--- smartmirror.py
+++ smartmirror.py
@@ -21,11 +21,11 @@ ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
 time_format = 12 # 12 or 24
 date_format = "%b %d, %Y" # check python doc for strftime() for options
 news_country_code = 'us'
-weather_api'<83c4f903da34ac9c15337f27a24c97aa>' # create account at https://darksky.net/dev/
+weather_api'83c4f903da34ac9c15337f27a24c97aa' # create account at https://darksky.net/dev/
 weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
 weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
-latitude = None # Set this if IP location lookup does not work for you (must be a string)
-longitude = None # Set this if IP location lookup does not work for you (must be a string)
+latitude = 39.7684# Set this if IP location lookup does not work for you (must be a string)
+longitude = 86.1581 # Set this if IP location lookup does not work for you (must be a string)
 xlarge_text_size = 94
 large_text_size = 48
 medium_text_size = 28
